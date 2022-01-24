1054fa68841107da43809fc3f8b457bdb9f3cbd5
RunSpaceSoftware <shayanfr1387@gmail.com>
2022-01-24 16:47:13 +0330

    Django

--- WebAppPy/urls.py
+++ WebAppPy/urls.py
@@ -1,4 +1,4 @@
-"""webapppy URL Configuration
+"""WebAppPy URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
